9cfb690788e776ee8b51fdfae846475797180e8d
liya-zhu <julia.liya.zhu@gmail.com>
2020-12-29 16:57:18 -0500

    what eveer

---
+++ S1_16_flipper.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class S1_16_flipper {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int a=1;
+        int b=2;
+        int c=3;
+        int d=4;
+        String or=scan.next();
+        for (int i=0; i<or.length(); i++){
+            if (or.charAt(i)=='H'){
+                int temp=a;
+                a=c;
+                c=temp;
+                temp=b;
+                b=d;
+                d=temp;
+            }
+            if (or.charAt(i)=='V'){
+                int temp=a;
+                a=b;
+                b=temp;
+                temp=c;
+                c=d;
+                d=temp;
+            }
+        }
+        System.out.println(a+" "+b);
+        System.out.println(c+" "+d);
+    }
+}
---
+++ S1_17_village.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class S1_17_village {
+    static Scanner scan=new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = scan.nextInt();
+        int[] villages = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            villages[i]=scan.nextInt();
+        }
+        Arrays.sort(villages);
+        double size=Integer.MAX_VALUE;
+
+        for(int i=1; i<n-1;i++){
+            double temp=(villages[i]-villages[i-1])*1.0/2+(villages[i+1]-villages[i])*1.0/2;
+            if (temp<size){
+                size=temp;
+            }
+        }
+        System.out.println(Math.round(size*10)/10.0);
+    }
+}
